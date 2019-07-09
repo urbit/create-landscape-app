4c6a36e61aa39330b077371717d3e37b825651ad
Matilde Park <matilde@park.computer>
2019-07-09 13:32:02 -0700

    Capital juggling

--- install.js
+++ install.js
@@ -7,24 +7,31 @@ prompt.colors = false
 prompt.get([{
     name: 'appName',
     required: true,
-    description: "What's the name of your application? No spaces, please."
+    description: "What's the name of your application? Lowercase and no spaces, please."
     }, {
     name: 'pier',
     required: true,
     description: "Where is your Urbit pier located? For example, '/Users/dev/zod/home'"
     }], function (err, result) {
         fs.renameSync('urbit/app/smol.hoon', 'urbit/app/' + result.appName + '.hoon')
+        let capitalisedAppName = result.appName.charAt(0).toUpperCase() + result.appName.slice(1)
         const appNameOptions = {
-            files: ['gulpfile.js', 'tile/tile.js', 'urbit/app/' + result.appName + '.hoon'],
+            files: ['gulpfile.js', 'urbit/app/' + result.appName + '.hoon'],
             from: /%APPNAME%/g,
             to: result.appName
             }
+        const appNamewithCapitals = {
+            files: 'tile/tile.js',
+            from: /%APPNAME%/g,
+            to: [capitalisedAppName, result.appName, capitalisedAppName, result.appName, capitalisedAppName]
+        }
         const urbitPierOptions = {
             files: '.urbitrc',
             from: "%URBITPIER%",
             to: result.pier
         }
         replace(appNameOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
+        replace(appNamewithCapitals).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
         replace(urbitPierOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
         console.log("All done! Happy hacking.")
     }
