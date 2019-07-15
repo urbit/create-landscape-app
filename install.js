84c724fb1c77c1491f451103736e5ba29794422c
Matilde Park <matilde@park.computer>
2019-07-15 15:04:54 -0700

    handcarving callbacks

--- install.js
+++ install.js
@@ -123,7 +123,7 @@ var movePromiser = function (from, to, records) {
         });
 };
 
-var moveDir = function (from_dir, to_dir) {
+var moveDir = function (from_dir, to_dir, callback) {
     return fs.readdir(from_dir)
         .then(function (children) {
             return fs.ensureDir(to_dir)
@@ -154,7 +154,7 @@ var moveDir = function (from_dir, to_dir) {
                         });
                 }).then(function () {
                     return fs.rmdir(from_dir);
-                });
+                }).then(callback);
         });
 };
 
@@ -162,25 +162,26 @@ const setupFull = function (result) {
     deleteFolderRecursive('tile')
     deleteFolderRecursive('urbit')
     fs.unlinkSync('gulpfile.js')
-    moveDir('full', '/')
-    fs.renameSync('urbit/app/smol.hoon', 'urbit/app/' + result.appName + '.hoon')
-    moveDir('urbit/app/smol/', 'urbit/app/' + result.appName)
-    moveDir('urbit/mar/smol/', 'urbit/mar/' + result.appName)
-    fs.renameSync('urbit/lib/smol.hoon', 'urbit/lib/' + result.appName + '.hoon')
-    let urbitPierOptions = {
-        files: '.urbitrc',
-        from: "%URBITPIER%",
-        to: result.pier
-    }
-    replace(urbitPierOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
-    let appNameOptions = {
-        files: ['gulpfile.js', 'urbit/app/' + result.appName + '.hoon', 'tile/tile.js', 
-        'src/js/api.js', 'src/js/subscription.js', 'src/js/components/root.js',
-        'urbit/mar' + result.appName + '/action.hoon', 'urbit/mar/' + result.appName + '/update.hoon',
-        'urbit/mar/' + result.appName + '/config.hoon', 'urbit/lib/' + result.appName + '.hoon'
-        ],
-        from: /%APPNAME%/g,
-        to: result.appName
-    }
-    replace(appNameOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
+    moveDir('full', '/', function() {
+        fs.renameSync('urbit/app/smol.hoon', 'urbit/app/' + result.appName + '.hoon')
+        moveDir('urbit/app/smol/', 'urbit/app/' + result.appName)
+        moveDir('urbit/mar/smol/', 'urbit/mar/' + result.appName)
+        fs.renameSync('urbit/lib/smol.hoon', 'urbit/lib/' + result.appName + '.hoon')
+        let urbitPierOptions = {
+            files: '.urbitrc',
+            from: "%URBITPIER%",
+            to: result.pier
+        }
+        replace(urbitPierOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
+        let appNameOptions = {
+            files: ['gulpfile.js', 'urbit/app/' + result.appName + '.hoon', 'tile/tile.js', 
+            'src/js/api.js', 'src/js/subscription.js', 'src/js/components/root.js',
+            'urbit/mar' + result.appName + '/action.hoon', 'urbit/mar/' + result.appName + '/update.hoon',
+            'urbit/mar/' + result.appName + '/config.hoon', 'urbit/lib/' + result.appName + '.hoon'
+            ],
+            from: /%APPNAME%/g,
+            to: result.appName
+        }
+        replace(appNameOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
+    })
 }
