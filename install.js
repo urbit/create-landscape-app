e1c23078f3cd76763d9a52225844b6090414e9a1
Matilde Park <matilde@park.computer>
2019-07-15 14:54:58 -0700

    fingers crossed

--- install.js
+++ install.js
@@ -1,7 +1,8 @@
 const prompt = require('prompt')
 const replace = require('replace-in-file')
-const fs = require('fs')
-const fse = require('fs-extra');
+const fs = require('fs-extra');
+var Promise = require('promise');
+var path = require('path');
 
 // Making the text input a bit legible.
 
@@ -78,31 +79,108 @@ const deleteFolderRecursive = function (path) {
     }
 };
 
+var promiseAllWait = function (promises) {
+    // this is the same as Promise.all(), except that it will wait for all promises to fulfill before rejecting
+    var all_promises = [];
+    for (var i_promise = 0; i_promise < promises.length; i_promise++) {
+        all_promises.push(
+            promises[i_promise]
+                .then(function (res) {
+                    return { res: res };
+                }).catch(function (err) {
+                    return { err: err };
+                })
+        );
+    }
+
+    return Promise.all(all_promises)
+        .then(function (results) {
+            return new Promise(function (resolve, reject) {
+                var is_failure = false;
+                var i_result;
+                for (i_result = 0; i_result < results.length; i_result++) {
+                    if (results[i_result].err) {
+                        is_failure = true;
+                        break;
+                    } else {
+                        results[i_result] = results[i_result].res;
+                    }
+                }
+
+                if (is_failure) {
+                    reject(results[i_result].err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+};
+
+var movePromiser = function (from, to, records) {
+    return fs.move(from, to)
+        .then(function () {
+            records.push({ from: from, to: to });
+        });
+};
+
+var moveDir = function (from_dir, to_dir) {
+    return fs.readdir(from_dir)
+        .then(function (children) {
+            return fs.ensureDir(to_dir)
+                .then(function () {
+                    var move_promises = [];
+                    var moved_records = [];
+                    var child;
+                    for (var i_child = 0; i_child < children.length; i_child++) {
+                        child = children[i_child];
+                        move_promises.push(movePromiser(
+                            path.join(from_dir, child),
+                            path.join(to_dir, child),
+                            moved_records
+                        ));
+                    }
+
+                    return promiseAllWait(move_promises)
+                        .catch(function (err) {
+                            var undo_move_promises = [];
+                            for (var i_moved_record = 0; i_moved_record < moved_records.length; i_moved_record++) {
+                                undo_move_promises.push(fs.move(moved_records[i_moved_record].to, moved_records[i_moved_record].from));
+                            }
+
+                            return promiseAllWait(undo_move_promises)
+                                .then(function () {
+                                    throw err;
+                                });
+                        });
+                }).then(function () {
+                    return fs.rmdir(from_dir);
+                });
+        });
+};
+
 const setupFull = function (result) {
     deleteFolderRecursive('tile')
     deleteFolderRecursive('urbit')
     fs.unlinkSync('gulpfile.js')
-    fse.move('full/*', '', {overwrite: true}, function(err) {
-            if (err) return console.error(err)
-            fs.renameSync('urbit/app/smol.hoon', 'urbit/app/' + result.appName + '.hoon')
-            fse.move('urbit/app/smol/', 'urbit/app/' + result.appName, function(err) {})
-            fse.move('urbit/mar/smol/', 'urbit/mar/' + result.appName, function (err) {})
-            fs.renameSync('urbit/lib/smol.hoon', 'urbit/lib/' + result.appName + '.hoon')
-            let urbitPierOptions = {
-                files: '.urbitrc',
-                from: "%URBITPIER%",
-                to: result.pier
-            }
-            replace(urbitPierOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
-            let appNameOptions = {
-                files: ['gulpfile.js', 'urbit/app/' + result.appName + '.hoon', 'tile/tile.js', 
-                'src/js/api.js', 'src/js/subscription.js', 'src/js/components/root.js',
-                'urbit/mar' + result.appName + '/action.hoon', 'urbit/mar/' + result.appName + '/update.hoon',
-                'urbit/mar/' + result.appName + '/config.hoon', 'urbit/lib/' + result.appName + '.hoon'
-                ],
-                from: /%APPNAME%/g,
-                to: result.appName
-            }
-            replace(appNameOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
-    })
+    moveDir('full', '/')
+    fs.renameSync('urbit/app/smol.hoon', 'urbit/app/' + result.appName + '.hoon')
+    moveDir('urbit/app/smol/', 'urbit/app/' + result.appName)
+    moveDir('urbit/mar/smol/', 'urbit/mar/' + result.appName)
+    fs.renameSync('urbit/lib/smol.hoon', 'urbit/lib/' + result.appName + '.hoon')
+    let urbitPierOptions = {
+        files: '.urbitrc',
+        from: "%URBITPIER%",
+        to: result.pier
+    }
+    replace(urbitPierOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
+    let appNameOptions = {
+        files: ['gulpfile.js', 'urbit/app/' + result.appName + '.hoon', 'tile/tile.js', 
+        'src/js/api.js', 'src/js/subscription.js', 'src/js/components/root.js',
+        'urbit/mar' + result.appName + '/action.hoon', 'urbit/mar/' + result.appName + '/update.hoon',
+        'urbit/mar/' + result.appName + '/config.hoon', 'urbit/lib/' + result.appName + '.hoon'
+        ],
+        from: /%APPNAME%/g,
+        to: result.appName
+    }
+    replace(appNameOptions).then(changedFiles => console.log(changedFiles)).catch(err => console.error(err))
 }
