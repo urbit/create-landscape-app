c710a973be0979f2e92c26973664102d55d6753e
Matilde Park <matilde@park.computer>
2019-07-22 23:05:17 -0400

    Nudging the tile on the left.

--- tile/tile.js
+++ tile/tile.js
@@ -9,7 +9,7 @@ export default class %APPNAME%Tile extends Component {
     return (
       <div className="w-100 h-100 relative" style={{ background: '#1a1a1a' }}>
           <p className="gray label-regular b absolute" style={{ left: 8, top: 4 }}>%APPNAME%</p>
-        <p className="white absolute" style={{ top: 25 }}>Welcome to your app, {this.props.data}.</p>
+        <p className="white absolute" style={{ top: 25, left: 8 }}>Welcome to your app, {this.props.data}.</p>
       </div>
     );
   }
