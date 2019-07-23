01a345c42e109e45edfcfa4d32419fc36e061564
Matilde Park <matilde@park.computer>
2019-07-23 15:47:29 -0400

    Bringing full's tile to parity with tile style

--- full/tile/tile.js
+++ full/tile/tile.js
@@ -10,7 +10,7 @@ export default class %APPNAME%Tile extends Component {
       <div className="w-100 h-100 relative" style={{ background: '#1a1a1a' }}>
         <a className="w-100 h-100 db pa2 no-underline" href="/~%APPNAME%">
           <p className="gray label-regular b absolute" style={{ left: 8, top: 4 }}>%APPNAME%</p>
-          <p className="white absolute" style={{top: 25}}>This is sample text for your full app tile.</p>
+          <p className="white absolute" style={{ top: 25, left: 8 }}>This is sample text for your full app tile.</p>
         </a>
       </div>
     );
