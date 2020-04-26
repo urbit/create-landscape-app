92961afb5921632b4d9e2543fd0db6be3b77670b
John Franklin <jfranklin9000@gmail.com>
2020-04-26 16:27:29 -0500

    tile: make tile-only apps non-clickable

--- tile/tile.js
+++ tile/tile.js
@@ -7,9 +7,7 @@ export default class %APPNAME%Tile extends Component {
   render() {
     return (
       <div className="w-100 h-100 relative bg-white bg-gray0-d ba b--black b--gray1-d">
-        <a className="w-100 h-100 db pa2 no-underline" href="/~%APPNAME%">
-          <p className="black white-d absolute f9" style={{ left: 8, top: 8 }}>%APPNAME%</p>
-        </a>
+        <p className="black white-d absolute f9" style={{ left: 8, top: 8 }}>%APPNAME%</p>
       </div>
     );
   }
