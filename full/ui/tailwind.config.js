39a410db98855ebc20b8b2112a8db71e4737e472
Hunter Miller <hunter@thisdot.co>
2021-11-04 13:46:33 -0500

    removing fonts and linting/formatting

--- full/ui/tailwind.config.js
+++ full/ui/tailwind.config.js
@@ -3,19 +3,6 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: [
-        'Inter',
-        'Inter UI',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'San Francisco',
-        'Helvetica Neue',
-        'Arial',
-        'sans-serif'
-      ],
-      mono: ['Source Code Pro', 'Roboto mono', 'Courier New', 'monospace']
-    },
     extend: {}
   },
   screens: {},
