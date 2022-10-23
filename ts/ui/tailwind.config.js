8b0c3dbfffa51ece8a6d64836d9bf7c798e158de
Matilde Park <m@park.computer>
2022-10-23 10:02:23 -0700

    build: upgrade to tailwind 3
    Fixes #74

--- ts/ui/tailwind.config.js
+++ ts/ui/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
